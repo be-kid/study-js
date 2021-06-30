95b6cb4cd60986158838098e6738d9fa38d11ad4
daramzy <daramzycoder@gmail.com>
2021-06-30 18:49:24 +0900

    210630 study

---
+++ app.js
@@ -0,0 +1,26 @@
+/*
+alert("hi");
+*/
+console.log(34523465);
+//로그에 출력하기
+
+//변수 만들기
+const a = 5;    // const는 값을 바꿀 수 없음
+let b = 2;      // let은 값을 바꿀 수 있음, 업데이트 가능
+// 업데이트할 때는 let을 붙이지 않음
+let myName = "legu";
+
+console.log(a+b);
+
+b = 3;
+
+console.log(a*b);
+console.log(a/b);
+
+myName = "daramzy";
+
+console.log("hello "+myName);
+
+//옛날엔 var로 변수 선언했는데 변수 보호가 안되기때문에 const와 let을 만들었다.
+//여전히 사용은 가능하다.
+//주로 const를 쓰고 let은 필요할 때, var은 쓰지 않는다.
